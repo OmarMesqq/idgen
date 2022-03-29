73d7c81c07ebd874c6d1d884e4b34299b6b7321c
Omar <34662089+OmarMesqq@users.noreply.github.com>
2022-03-29 19:43:55 -0300

    Conserto de formatação

--- app/src/main/java/com/mesq/idgen/MainActivity.kt
+++ app/src/main/java/com/mesq/idgen/MainActivity.kt
@@ -17,8 +17,7 @@ class MainActivity : AppCompatActivity() {
         botaoPrincipal.setOnClickListener {
 
 
-            val alg1: Int =
-                (1..9).random()        // gera os  9 primeiros dígitos do CPF de forma "aleatória"
+            val alg1: Int = (1..9).random()        // gera os  9 primeiros dígitos do CPF de forma "aleatória"
             val alg2: Int = (1..9).random()
             val alg3: Int = (1..9).random()
             val alg4: Int = (1..9).random()
@@ -30,8 +29,7 @@ class MainActivity : AppCompatActivity() {
 
             val t1 = alg1.times(10)             // Primeira etapa para determinar o primeiro
             val t2 = alg2.times(9)              // dígito verificador (d1):
-            val t3 =
-                alg3.times(8)              // multiplicar os 9 primeiros dígitos por 10,9,8,...,2
+            val t3 = alg3.times(8)              // multiplicar os 9 primeiros dígitos por 10,9,8,...,2
             val t4 = alg4.times(7)
             val t5 = alg5.times(6)
             val t6 = alg6.times(5)
@@ -44,8 +42,8 @@ class MainActivity : AppCompatActivity() {
             val r: Int = (t1 + t2 + t3 + t4 + t5 + t6 + t7 + t8 + t9) % 11
 
             var d1 = 50 // inicializei o dígito verificador com valor arbitrário que será
-            // modificado pela lógica do CPF
-            // olhar a função detD1 na MainActivity para entender
+                        // modificado pela lógica do CPF
+                        // olhar a função detD1 na MainActivity para entender
 
             fun detD1() {
                 d1 = when (r) {
@@ -56,13 +54,11 @@ class MainActivity : AppCompatActivity() {
             }
             detD1()
 
-            val z1 = alg2.times(10)      // Primeira etapa para determinar o segundo
-            val z2 = alg3.times(9)       // dígito verificador (d2):
-            val z3 =
-                alg4.times(8)       // multiplicar os 9 dígitos a partir do segundo pela sequência
-            val z4 =
-                alg5.times(7)       // 10,9,8,...,2, tendo d1, calculado na primeira parte, como
-            val z5 = alg6.times(6)       // o último fator da multiplicação
+            val z1 = alg2.times(10)         // Primeira etapa para determinar o segundo
+            val z2 = alg3.times(9)          // dígito verificador (d2):
+            val z3 = alg4.times(8)          // multiplicar os 9 dígitos a partir do segundo pela sequência
+            val z4 = alg5.times(7)          // 10,9,8,...,2, tendo d1, calculado na primeira parte, como
+            val z5 = alg6.times(6)          // o último fator da multiplicação
             val z6 = alg7.times(5)
             val z7 = alg8.times(4)
             val z8 = alg9.times(3)
@@ -74,7 +70,7 @@ class MainActivity : AppCompatActivity() {
             val s: Int = (z1 + z2 + z3 + z4 + z5 + z6 + z7 + z8 + z9) % 11
 
             var d2 = 60 //Assim como fiz com d1, inicializei o segundo dígito verificador
-            // com um valor qualquer que será modificado na função detD2()
+                        // com um valor qualquer que será modificado na função detD2()
 
 
             fun detD2() {
